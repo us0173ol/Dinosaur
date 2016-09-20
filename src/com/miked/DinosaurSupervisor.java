d5ce426ee13610491fdb227f934c07efd4c8bdc6
miked <whatthafunk3@aol.com>
2016-09-20 11:34:16 -0500

    added two more dinosaurs that roar and kill

--- src/com/miked/DinosaurSupervisor.java
+++ src/com/miked/DinosaurSupervisor.java
@@ -6,12 +6,17 @@ public class DinosaurSupervisor {
 	Dinosaur t_rex = new Dinosaur();
     Dinosaur velociraptor = new Dinosaur();
     Dinosaur diplodocus = new Dinosaur();
+    Dinosaur triceratops = new Dinosaur();
 
 
     t_rex.roar();
     velociraptor.roar();
+    diplodocus.roar();
+    triceratops.roar();
     t_rex.kill();
     velociraptor.kill();
+    diplodocus.kill();
+    triceratops.kill();
     }
 }
 
